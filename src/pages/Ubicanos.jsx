3e486166e031446ad190823a332daeba60a8592d
Carlos Albino <92502513+Meka404@users.noreply.github.com>
2023-10-14 13:03:19 -0500

    mapa cambiado a uno mejor

--- src/pages/Ubicanos.jsx
+++ src/pages/Ubicanos.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import "../css/Footer.css";
 import { Footer } from "../components/Footer";
 import { Direccion1 } from "../components/Direccion1";
-import {Mapa} from "../components/Mapa";
+
 import "../css/Empresas.css";
 export function Ubicanos() {
   return (
     <div>
-   
       <Direccion1></Direccion1>
-
+      
       <Footer></Footer>
     </div>
   );
